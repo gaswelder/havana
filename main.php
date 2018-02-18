497f0b6b2522c30e39fc67270faf10eb3bdc1cad
gas <gasweld@gmail.com>
2018-02-18 19:14:41 +0300

    Moved some files

--- main.php
+++ main.php
@@ -1,25 +1,57 @@
 <?php
+use havana\dbclient;
 
-function panic($message) {
+function panic($message)
+{
 	throw new Exception($message);
+}
+
+/**
+ * @return dbclient
+ */
+function db($url = null)
+{
+	static $clients = [];
+
+	if (!$url) {
+		$url = getenv('DATABASE');
+	}
+	if (!$url) {
+		throw new Exception("Missing DATABASE env var");
+	}
+	if (!isset($clients[$url])) {
+		$clients[$url] = dbclient::make($url);
+	}
+	return $clients[$url];
+}
+
+function dd()
+{
+	call_user_func_array('var_dump', func_get_args());
+	exit;
+}
+
+function dump()
+{
+	call_user_func_array('var_dump', func_get_args());
 }
 
 set_error_handler(function ($errno, $msg, $path, $line, $context) {
 	panic("$msg at $path:$line");
 });
 
-require __DIR__.'/app.php';
-require __DIR__.'/lang.php';
-require __DIR__.'/mail.php';
-require __DIR__.'/match.php';
-require __DIR__.'/response.php';
-require __DIR__.'/request.php';
-require __DIR__.'/upload.php';
-require __DIR__.'/db/db.php';
-require __DIR__.'/db/dbobject.php';
-require __DIR__.'/tpl.php';
-require __DIR__.'/user.php';
-require __DIR__.'/func.php';
+require __DIR__ . '/app.php';
+require __DIR__ . '/lang.php';
+require __DIR__ . '/mail.php';
+require __DIR__ . '/response.php';
+require __DIR__ . '/request.php';
+require __DIR__ . '/upload.php';
+require __DIR__ . '/db/db.php';
+require __DIR__ . '/db/dbobject.php';
+require __DIR__ . '/tpl.php';
+require __DIR__ . '/url.php';
+require __DIR__ . '/user.php';
 
-require __DIR__.'/private/env.php';
-require __DIR__.'/private/mime.php';
+require __DIR__ . '/private/env.php';
+require __DIR__ . '/private/match.php';
+require __DIR__ . '/private/mime.php';
--- request.php
+++ request.php
@@ -1,40 +1,6 @@
 <?php
 namespace havana;
 
-class url
-{
-	private $data;
-	private $original;
-
-	function __construct($url)
-	{
-		$this->original = $url;
-		$this->data = parse_url($url);
-
-		$domain = "$this->scheme://$this->host";
-		if ($this->port) {
-			$domain .= ":$this->port";
-		}
-		$this->data['domain'] = $domain;
-	}
-
-	function __toString()
-	{
-		return $this->original;
-	}
-
-	function __get($k)
-	{
-		return isset($this->data[$k]) ? $this->data[$k] : null;
-	}
-
-	function isUnder($prefix)
-	{
-		$path = $this->path;
-		return $path == $prefix || strpos($path, $prefix . '/') === 0;
-	}
-}
-
 class request
 {
 	private static $init = false;
